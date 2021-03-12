aca460bf72ff1825e95bb32a415a05245afb1372
ReS <43923010+Ryoukage@users.noreply.github.com>
2021-03-12 09:46:32 +0700

    Update Bullet.h

--- src/Bullet.h
+++ src/Bullet.h
@@ -7,16 +7,21 @@ class Bullet
 {
 public:
 	Bullet();
-	Bullet(float l_radius, sf::Vector2f l_position, sf::Color l_color, sf::Vector2f l_velocity);
+	Bullet(float l_radius, sf::Vector2f l_position, sf::Color l_color, sf::Vector2f l_velocity,sf::Text l_text);
 	~Bullet();
 
-	void Update();
+	void Update(sf::Time time);
 
 	void SetOutOfLayer(bool l_layer);
 	bool GetOutOfLayer()const;
 
 	void SetCollision(bool l_collision);
 	bool GetCollision()const;
+
+	void SetMiss(bool l_miss);
+	void SetColor(sf::Color l_color);
+
+	sf::Text* GetText();
 	
 	sf::CircleShape* GetShape();
 	sf::Vector2f GetVelocity()const;
@@ -25,9 +30,16 @@ private:
 	sf::CircleShape m_bullet;
 
 	sf::Vector2f m_velocity;
+	
+	sf::Text m_text;
+
+	sf::Color m_color;
+
+	float m_time;
 
 	bool m_outOfLayer;
 
 	bool m_isCollision;
+	bool m_miss;
 };
 
