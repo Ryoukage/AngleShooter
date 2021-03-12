9d1aca3506f0c0c4daf62efa3cc3ff5bbc548c74
ReS <43923010+Ryoukage@users.noreply.github.com>
2021-03-12 09:45:46 +0700

    Update Bullet.cpp

--- src/Bullet.cpp
+++ src/Bullet.cpp
@@ -2,14 +2,16 @@
 
 Bullet::Bullet()
 {
-
+	
 }
 
-Bullet::Bullet(float l_radius, sf::Vector2f l_position, sf::Color l_color, sf::Vector2f l_velocity) :m_velocity(l_velocity), m_outOfLayer(false), m_isCollision(false)
+Bullet::Bullet(float l_radius, sf::Vector2f l_position, sf::Color l_color, sf::Vector2f l_velocity, sf::Text l_text) :m_velocity(l_velocity), m_outOfLayer(false), m_isCollision(false), m_time(1.f), m_miss(false)
 {
 	m_bullet.setRadius(l_radius);
 	m_bullet.setPosition(l_position);
 	m_bullet.setFillColor(l_color);
+	
+	m_text = l_text;
 }
 
 sf::CircleShape* Bullet::GetShape()
@@ -37,9 +39,34 @@ bool Bullet::GetCollision()const
 	return m_isCollision;
 }
 
-void Bullet::Update()
+void Bullet::SetMiss(bool l_miss)
 {
-	
+	m_miss = l_miss;
+}
+
+void Bullet::SetColor(sf::Color l_color)
+{
+	m_color = l_color;
+}
+
+void Bullet::Update(sf::Time time)
+{
+	if (m_miss)
+	{
+		m_time -= time.asSeconds();
+		m_color.a = 255 * m_time;
+		m_text.setFillColor(m_color);
+		m_text.move(0.f, -250.f * time.asSeconds());
+		if (m_time < 0.f)
+		{
+			m_miss = false;
+		}
+	}
+}
+
+sf::Text* Bullet::GetText()
+{
+	return &m_text;
 }
 
 bool Bullet::GetOutOfLayer()const
